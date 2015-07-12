2f543c201cc6e211a4a09658ce6e1905c89b9439
claudealandry <claudealandry@gmail.com>
2015-07-12 17:11:59 -0400

    plot 3 and codeplot3 revision

--- codeplot3.R
+++ codeplot3.R
@@ -15,7 +15,7 @@ plot3 <- function() {
     plot(df1$datetime, df1$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
     lines(df1$datetime, df1$Sub_metering_2, col = "red")
     lines(df1$datetime, df1$Sub_metering_3, col = "blue")
-    legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty = c(1,1), lwd = c(1,1))
+    legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty = c(1,1), lwd = c(1,1), cex = .5)
     dev.copy(png, filename = "plot3.png", width = 480, height = 480)
     cat("plot3.png", getwd())
     dev.off()
